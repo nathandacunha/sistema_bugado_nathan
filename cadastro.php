234acecd3487312dfbeec7cc20b24eae0d699e3d
nathandacunha <nathan_cunha@estudante.sesisenai.org.br>
2025-11-28 07:29:45 -0300

    fix(cadastro): campo nome e cpf estao como obrigatorio

--- cadastro.php
+++ cadastro.php
@@ -21,11 +21,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <body>
   <h1>Cadastrar Cliente</h1>
   <form method="post">
-    <label>Nome:<br><input type="text" name="nome"></label><br><br>
-    <label>CPF:<br><input type="text" name="cpf"></label><br><br>
+    <label>Nome:<br><input type="text" name="nome" required></label><br><br>
+    <label>CPF:<br><input type="text" name="cpf" required></label><br><br>
     <button type="">Salvar</button>
   </form>
 
-  <p><a href="index">Voltar</a></p>
+  <p><a href="index.php">Voltar</a></p>
 </body>
 </html>
